89038f88fbfd72ff21d3b3d916bdce1e4bdf2ac7
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-11-28 16:17:13 +0100

    Added first function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,17 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert(`The result is ${result}`);
+}
+
+add();
+
+currentResult = currentResult + 10;
+
+let tempLiteralTest = 11;
+
+let outputResult1 = `this is template literal value ${tempLiteralTest}}`;
+
 outputResult(null, '');
