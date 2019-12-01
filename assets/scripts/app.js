2f33491142af82a620e3b19428034769aed5aa81
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-12-01 14:20:15 +0100

    Adding subtract, multiply and divide functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,4 +12,15 @@ function add() {
   outputResult(currentResult, calcDescription);
 }
 
+function substract() {
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} - ${enteredNumber}`;
+  currentResult = currentResult - enteredNumber;
+  outputResult(currentResult, calcDescription);
+}
+
+function multiply() {}
+
+function divide() {}
+
 addBtn.addEventListener('click', add);
