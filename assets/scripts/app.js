ecbd43bd18af550bfbcd276c21c749b4985b36de
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-12-02 17:02:51 +0100

    Completed subtract, multiply and divide functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,22 +5,40 @@ function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
 function add() {
   const enteredNumber = getUserNumberInput();
-  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  const initialResult = currentResult;
   currentResult = currentResult + enteredNumber;
-  outputResult(currentResult, calcDescription);
+  createAndWriteOutput('+', initialResult, enteredNumber);
 }
 
 function substract() {
   const enteredNumber = getUserNumberInput();
-  const calcDescription = `${currentResult} - ${enteredNumber}`;
+  const initialResult = currentResult;
   currentResult = currentResult - enteredNumber;
-  outputResult(currentResult, calcDescription);
+  createAndWriteOutput('-', initialResult, enteredNumber);
 }
 
-function multiply() {}
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+}
 
-function divide() {}
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
+}
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', substract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
