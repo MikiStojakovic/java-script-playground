c6a1b0049a5e20a9c47e61d8917a7f8c2c2053f9
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-02-04 16:38:09 +0100

    Added backdrop behavior logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,25 @@
 const addMovieModal = document.getElementById('add-modal');
-// const addMovieModal = document.querySelector('#add-modal');
-// const addMovieModal = document.body.children[1];
 const startAddMovieButton = document.querySelector('header button');
-// const startAddMovieButton = document.querySelector('header').lastElementChild;
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 
-const toggleMovieModal = () => { // function() {}
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
+
+const toggleMovieModal = () => {
   addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+const cancelAddMovieBtnClickHandler = () => {
+  toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieBtnClickHandler);
