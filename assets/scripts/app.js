d996e4324d4ea8e0026b6b3ed1072a2c739936ef
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-02-04 17:36:28 +0100

    Added add movie click button handler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,12 +2,14 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible');
 };
 
-const toggleMovieModal = () => {
+const toggleMovieModalHandler = () => {
   addMovieModal.classList.toggle('visible');
   toggleBackdrop();
 };
@@ -20,6 +22,23 @@ const cancelAddMovieBtnClickHandler = () => {
   toggleMovieModal();
 };
 
-startAddMovieButton.addEventListener('click', toggleMovieModal);
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imageUrlValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert('Please enter valid values (rating between 1 and 5).');
+  }
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModalHandler);
 backdrop.addEventListener('click', backdropClickHandler);
 cancelAddMovieButton.addEventListener('click', cancelAddMovieBtnClickHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
